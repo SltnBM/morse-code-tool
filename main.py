b5b2c4dd460cd95ec8e9595042f8b13cec59133d
SltnBM <SltnBM@users.noreply.github.com>
2025-09-22 19:18:54 +0700

    feat: handle ctrl+c with goodbye message

--- main.py
+++ main.py
@@ -28,20 +28,23 @@ def morse_to_text(morse):
         decoded_words.append("".join(chars))
     return " ".join(decoded_words)
 
-while True:
-    print("\n--- Morse Code Translator ---")
-    print("1. Text to Morse")
-    print("2. Morse to Text")
-    print("3. Exit")
-    choice = input("Choose (1/2/3): ")
+try:
+    while True:
+        print("\n--- Morse Code Translator ---")
+        print("1. Text to Morse")
+        print("2. Morse to Text")
+        print("3. Exit")
+        choice = input("Choose (1/2/3): ")
 
-    if choice == '1':
-        text = input("Enter text: ")
-        print("Morse Code:", text_to_morse(text))
-    elif choice == '2':
-        morse = input("Enter Morse code (letters: space, words: /): ")
-        print("Text:", morse_to_text(morse))
-    elif choice == '3':
-        break
-    else:
-        print("Invalid choice!")
+        if choice == '1':
+            text = input("Enter text: ")
+            print("Morse Code:", text_to_morse(text))
+        elif choice == '2':
+            morse = input("Enter Morse code (letters: space, words: /): ")
+            print("Text:", morse_to_text(morse))
+        elif choice == '3':
+            break
+        else:
+            print("Invalid choice!")
+except KeyboardInterrupt:
+    print("\nExiting, Good Bye.")
