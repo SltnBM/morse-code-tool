a0b6407865382f78b7315ebf24496492c3368941
SltnBM <SltnBM@users.noreply.github.com>
2025-09-22 19:16:24 +0700

    feat: add morse to text translation

--- main.py
+++ main.py
@@ -19,16 +19,29 @@ def text_to_morse(text):
         morse_words.append(" ".join(morse_chars))
     return " / ".join(morse_words)
 
+def morse_to_text(morse):
+    morse_dict_reversed = {v: k for k, v in MORSE_CODE_DICT.items()}
+    words = morse.split(" / ")
+    decoded_words = []
+    for word in words:
+        chars = [morse_dict_reversed.get(code, '') for code in word.split()]
+        decoded_words.append("".join(chars))
+    return " ".join(decoded_words)
+
 while True:
     print("\n--- Morse Code Translator ---")
     print("1. Text to Morse")
-    print("2. Exit")
-    choice = input("Choose (1/2): ")
+    print("2. Morse to Text")
+    print("3. Exit")
+    choice = input("Choose (1/2/3): ")
 
     if choice == '1':
         text = input("Enter text: ")
         print("Morse Code:", text_to_morse(text))
     elif choice == '2':
+        morse = input("Enter Morse code (letters: space, words: /): ")
+        print("Text:", morse_to_text(morse))
+    elif choice == '3':
         break
     else:
         print("Invalid choice!")
